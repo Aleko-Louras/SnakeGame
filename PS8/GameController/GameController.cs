e3789c0222f3bf125e4e42c5efb5f4ffc274c230
qpritchett <95952743+qpritchett@users.noreply.github.com>
2023-11-20 22:59:26 -0700

    and add UpdateArrived event

--- PS8/GameController/GameController.cs
+++ PS8/GameController/GameController.cs
@@ -1,30 +1,29 @@
 ﻿namespace GController;
-using System.Net;
 using NetworkUtil;
-using System.Net.Sockets;
 using System.Text.RegularExpressions;
-using System.Diagnostics;
+using WorldModel;
 using System.Text.Json;
 
-public class GameController
-{
-    private WorldModel.World theWorld;
+public class GameController {
+    private World theWorld;
     private string connectedPlayer;
-    bool recievedSetup;
-    int playerID;
+    private bool recievedSetup;
+    private int playerID;
 
-    public GameController()
-    {
-        theWorld = new WorldModel.World(2000);
+    public delegate void GameUpdateHandler();
+    public event GameUpdateHandler UpdateArrived;
+
+    public GameController() {
+        theWorld = new World(2000);
+        connectedPlayer = "";
+
     }
-    public void Connect(string serverAddress, string playerName)
-    {
+    public void Connect(string serverAddress, string playerName) {
         Networking.ConnectToServer(OnConnect, serverAddress, 11000);
         connectedPlayer = playerName;
     }
 
-    private void OnConnect(SocketState state)
-    {
+    private void OnConnect(SocketState state) {
 
         SocketState theServer = state;
         Networking.Send(state.TheSocket, connectedPlayer + "\n");
@@ -35,18 +34,13 @@ public class GameController
 
     }
 
-    private void RecieveMessage(SocketState state)
-    {
-        if (state.ErrorOccurred)
-        {
+    private void RecieveMessage(SocketState state) {
+        if (state.ErrorOccurred) {
             return;
         }
-        if (!recievedSetup)
-        {
+        if (!recievedSetup) {
             InitialSetup(state);
-        }
-        else
-        {
+        } else {
             UpdateFromServer(state);
         }
 
@@ -55,25 +49,22 @@ public class GameController
 
     }
 
-    private void InitialSetup(SocketState state)
-    {
+    private void InitialSetup(SocketState state) {
 
         string totalData = state.GetData();
         string[] parts = Regex.Split(totalData, @"(?<=[\n])");
-        lock (theWorld)
-        {
+        lock (theWorld) {
 
             // Player ID comes first, assign it
             playerID = int.Parse(parts[0]);
             theWorld.playerID = playerID;
             state.RemoveData(0, parts[0].Length);
-            theWorld = new WorldModel.World(int.Parse(parts[1]));
+            theWorld = new World(int.Parse(parts[1]));
             state.RemoveData(0, parts[1].Length);
 
             // Walls come second, write them
-            for (int i = 2; i < parts.Length - 1; i++)
-            {
-                WorldModel.Wall? nextWall = JsonSerializer.Deserialize<WorldModel.Wall>(parts[i])!;
+            for (int i = 2; i < parts.Length - 1; i++) {
+                Wall? nextWall = JsonSerializer.Deserialize<Wall>(parts[i])!;
 
                 theWorld.Walls.Add(nextWall.wall, nextWall);
                 state.RemoveData(0, parts[i].Length);
@@ -84,44 +75,32 @@ public class GameController
         Networking.GetData(state);
     }
 
-    private void UpdateFromServer(SocketState state)
-    {
+    private void UpdateFromServer(SocketState state) {
         // Parse the incoming message
         string totalData = state.GetData();
         string[] parts = Regex.Split(totalData, @"(?<=[\n])");
 
         // Loop through the snakes
-        lock (theWorld)
-        {
-            for (int i = 0; i < parts.Length - 1; i++)
-            {
+        lock (theWorld) {
+            for (int i = 0; i < parts.Length - 1; i++) {
                 // Check if the incoming object is a snake or a powerup
 
                 JsonDocument doc = JsonDocument.Parse(parts[i]);
-                if (doc.RootElement.TryGetProperty("snake", out _))
-                {
-                    WorldModel.Snake? nextSnake = JsonSerializer.Deserialize<WorldModel.Snake>(parts[i])!;
-                    if (nextSnake.died)
-                    {
+                if (doc.RootElement.TryGetProperty("snake", out _)) {
+                    Snake? nextSnake = JsonSerializer.Deserialize<Snake>(parts[i])!;
+                    if (nextSnake.died) {
                         state.RemoveData(0, parts[i].Length);
-                    }
-                    else
-                    {
+                    } else {
                         theWorld.Snakes[nextSnake.snake] = nextSnake;
                         state.RemoveData(0, parts[i].Length);
                     }
-                }
-                else if (doc.RootElement.TryGetProperty("power", out _))
-                {
-                    WorldModel.Powerup? nextPowerup = JsonSerializer.Deserialize<WorldModel.Powerup>(parts[i])!;
+                } else if (doc.RootElement.TryGetProperty("power", out _)) {
+                    Powerup? nextPowerup = JsonSerializer.Deserialize<Powerup>(parts[i])!;
 
-                    if (nextPowerup.died)
-                    {
+                    if (nextPowerup.died) {
                         theWorld.Powerups.Remove(nextPowerup.power); // maybe a problem?
                         state.RemoveData(0, parts[i].Length);
-                    }
-                    else
-                    {
+                    } else {
                         theWorld.Powerups[nextPowerup.power] = nextPowerup;
                         state.RemoveData(0, parts[i].Length);
                     }
@@ -129,6 +108,10 @@ public class GameController
             }
         }
 
+        // TODO: Notify the view that a new game world has arrived from the server
+        UpdateArrived?.Invoke();
+
+        // Continue the Networ loop
         Networking.GetData(state);
     }
 }

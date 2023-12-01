6f8d224659993676011d7442a0662810804664ad
Aleko-Louras <123419605+Aleko-Louras@users.noreply.github.com>
2023-12-01 14:42:35 -0700

    Add Server code

--- PS8/Server/Server.cs
+++ PS8/Server/Server.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.Net.NetworkInformation;
+using System.Xml.Serialization;
+using WorldModel;
+
+namespace Server;
+
+public class Server
+{
+    public static void Main(string[] args)
+    {
+        // Decode the XML Settings into the World Model
+        XmlSerializer xMLSerializer = new XmlSerializer(typeof(Settings));
+        using FileStream myFileStream = new FileStream("myFileName.xml", FileMode.Open);
+        // Call the Deserialize method and cast to the object type.
+        Settings settings = (Settings)xMLSerializer.Deserialize(myFileStream);
+
+        World theWorld = new World(settings.UniverseSize);
+        Console.WriteLine("The world size is: " + theWorld.Size);
+
+        ServerController server = new ServerController();
+        server.StartServer();
+
+        Console.WriteLine("Server running...");
+        Console.Read();
+
+
+    }
+
+} 
 
---
+++ PS8/Server/ServerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using NetworkUtil;
+
+namespace Server
+{
+	public class ServerController
+	{
+		private Dictionary<int, Socket> clients;
+		public ServerController()
+		{
+			clients = new Dictionary<int, Socket>();
+		}
+
+
+		public void StartServer()
+		{
+			Networking.StartServer(OnClientConnect, 11000);
+		}
+
+        private void OnClientConnect(SocketState state)
+		{
+			Console.WriteLine("Contact from client");
+	
+			lock (clients)
+			{
+				int playerID = (int)state.ID;
+				Console.WriteLine("New player connected with ID: " + playerID);
+                clients.Add(playerID, state.TheSocket);
+			}
+
+			state.OnNetworkAction = OnReceive;
+
+			Networking.GetData(state);
+		}
+
+		private void OnReceive(SocketState state)
+		{
+			Console.WriteLine("Received data");
+			Networking.GetData(state);
+		}
+
+    }
+}
+

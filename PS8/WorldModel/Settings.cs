dfb93289e60028f3625ab7aa98fb005355248c87
Aleko-Louras <123419605+Aleko-Louras@users.noreply.github.com>
2023-12-01 14:42:13 -0700

    Add Settings Class

---
+++ PS8/WorldModel/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace WorldModel
+{
+	public class Settings
+	{
+		public int UniverseSize { get; private set; }
+		public int RespawnRate{ get; private set; }
+        public int MSPerFrame { get; private set; }
+        public Wall[]? Walls { get; private set; }
+
+        public Settings()
+		{
+			this.UniverseSize = UniverseSize;
+			this.RespawnRate = RespawnRate;
+			this.MSPerFrame = MSPerFrame;
+			this.Walls = Walls;
+
+		}
+	}
+}
+

31d373c48e71574f315d12dc199bea3e02674d58
qpritchett <95952743+qpritchett@users.noreply.github.com>
2023-12-04 14:46:55 -0700

    Update Server.cs

--- PS8/Server/Server.cs
+++ PS8/Server/Server.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Net.NetworkInformation;
-using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.Xml;
 using WorldModel;
 
 namespace Server;
@@ -10,11 +11,19 @@ public class Server
     public static void Main(string[] args)
     {
         // Decode the XML Settings into the World Model
-        XmlSerializer xMLSerializer = new XmlSerializer(typeof(Settings));
-        using FileStream myFileStream = new FileStream("settings.xml", FileMode.Open);
-
-        // Call the Deserialize method and cast to the object type.
-        Settings settings = (Settings)xMLSerializer.Deserialize(myFileStream);
+        DataContractSerializer ser = new(typeof(GameSettings));
+        string relativePath = Path.Combine("..", "..", "..", "..", "settings.xml");
+        FileStream fs = new FileStream(relativePath, FileMode.Open);
+        XmlDictionaryReader reader =
+                XmlDictionaryReader.CreateTextReader(fs, new XmlDictionaryReaderQuotas());
+        
+        GameSettings settings = (GameSettings)ser.ReadObject(reader, true)!;
+        reader.Close();
+        fs.Close();
+        Console.WriteLine(settings.UniverseSize);
+        Console.WriteLine(settings.MSPerFrame);
+        Console.WriteLine(settings.RespawnRate);
+        Console.WriteLine(settings.Walls);
 
         World theWorld = new World(settings.UniverseSize);
         Console.WriteLine("The world size is: " + theWorld.Size);

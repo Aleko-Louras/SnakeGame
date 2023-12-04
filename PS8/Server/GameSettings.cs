e13b8618d821c834dce9de0b5119cf94ffdd5853
Aleko-Louras <123419605+Aleko-Louras@users.noreply.github.com>
2023-12-04 15:55:34 -0700

    Move GameSettings

---
+++ PS8/Server/GameSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+namespace WorldModel
+{
+    [DataContract(Namespace = "")]
+    public class GameSettings
+	{
+		[DataMember(Name ="UniverseSize")]
+		public int UniverseSize { get;  set; }
+        [DataMember(Name = "RespawnRate")]
+        public int RespawnRate{ get;  set; }
+        [DataMember(Name = "MSPerFrame")]
+        public int MSPerFrame { get;  set; }
+
+        //TODO: Fix wall deserialization
+        [DataMember(Name = "Walls")]
+        public List<Wall> Walls { get; private set; }
+
+        public GameSettings()
+		{ 
+		}
+	}
+}
+
--- PS8/WorldModel/Wall.cs
+++ PS8/WorldModel/Wall.cs
@@ -1,30 +1,37 @@
 ﻿using System;
+using System.Runtime.Serialization;
+using System.Xml.Linq;
 using SnakeGame;
 namespace WorldModel
 {
-	/// <summary>
-	/// A class representing a wall object that is drawn in the snake game.
-	/// </summary>
-	public class Wall
+    /// <summary>
+    /// A class representing a wall object that is drawn in the snake game.
+    /// </summary>
+    [DataContract(Name = "Wall", Namespace = "")]
+    public class Wall
 	{
-		/// <summary>
-		/// a field for the walls unique ID
-		/// </summary>
+        /// <summary>
+        /// a field for the walls unique ID
+        /// </summary>
+        [DataMember(Name = "ID")]
         public int wall { get; private set; }
-		/// <summary>
-		/// A vector location for the first point of the wall.
-		/// </summary>
-		public Vector2D p1 { get; private set; }
-		/// <summary>
-		/// A vector location for the second point of the wall.
-		/// </summary>
+        /// <summary>
+        /// A vector location for the first point of the wall.
+        /// </summary>
+        [DataMember(Name = "p1")]
+        public Vector2D p1 { get; private set; }
+        /// <summary>
+        /// A vector location for the second point of the wall.
+        /// </summary>
+        [DataMember(Name = "p2")]
         public Vector2D p2 { get; private set; }
-		/// <summary>
-		/// A constructor for the wall object, setting values to each value passed in.
-		/// </summary>
-		/// <param name="wall"></param>
-		/// <param name="p1"></param>
-		/// <param name="p2"></param>
+        /// <summary>
+        /// A constructor for the wall object, setting values to each value passed in.
+        /// </summary>
+        /// <param name="wall"></param>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        
 		public Wall(int wall, Vector2D p1, Vector2D p2)
 		{
 			this.wall = wall;
